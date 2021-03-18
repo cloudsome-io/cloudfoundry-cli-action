9872b4d945554cb97a30db80bc8ce31dbace25cf
Elliott Pope <epope@bu.edu>
2021-03-18 14:24:26 -0400

    Updating the entrypoint script to use the new input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 cf api "$INPUT_CF_API"
 cf auth "$INPUT_USERNAME" "$INPUT_PASSWORD"
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_SPACE" ]; then
+if [ -n "$INPUT_ORG" ] && [ -n "$INPUT_SPACE" ]; then
   cf target -o "$INPUT_ORG" -s "$INPUT_SPACE"
 fi
 
