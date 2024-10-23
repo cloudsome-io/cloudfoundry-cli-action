8555d59663bd61a2641c13786a611a335bf04221
michele <michele.pini@cloudsome.io>
2024-10-23 16:47:27 +0200

    updating the cli version and adding the stack parameters as optional

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:latest
 RUN apk update
 RUN apk add --no-cache curl jq bash ca-certificates
 
-ENV CF_CLI_VERSION "7.2.0"
+ENV CF_CLI_VERSION "7.7.14"
 
 RUN curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&version=${CF_CLI_VERSION}&source=github-rel" | tar -zx -C /usr/local/bin
 
--- blue-green.sh
+++ blue-green.sh
@@ -6,7 +6,14 @@ fi
 if cf app $1 > /dev/null 2>&1 ; then
   cf rename $1 $1-old
 fi
-cf push
+
+echo "Pushing $1"
+if [ -n "$2" ]; then
+  cf push -f ./manifest.yml -s $2
+else
+  cf push -f ./manifest.yml
+fi
+
 if cf app $1 > /dev/null 2>&1 ; then
   if [ `cf curl /v2/apps/\`cf app $1 --guid\`/stats | jq -r '."0".state'` == "RUNNING" ] ; then
     if cf app $1-old > /dev/null 2>&1 ; then
